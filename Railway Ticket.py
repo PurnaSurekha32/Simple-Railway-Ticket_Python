c60048553a8b61ecf042aa0873f8d82a2c5e7e1c
PurnaSurekha32 <purna32surekha@gmail.com>
2024-07-19 19:46:30 +0530

    Add files via upload

---
+++ Railway Ticket.py
@@ -0,0 +1,47 @@
+
+#Railway Ticket
+'''while True:
+    Ticket=1000
+    a=int(input(Gender :
+            1.Male
+            2.Female
+            Choose the gender:))
+    c=int(input("Enter your age: "))
+
+    if a==1 and c>=60:
+        print("Your Ticket price has 30% discount :",(Ticket)-(0.3)*Ticket)
+    elif a==1 and c<60:
+        print("Your Ticket price has 0% discount : ",Ticket)
+    elif a==2 and c>=60:
+        print("Your Ticket price has 50% discount : ", (Ticket)-(0.5)*Ticket)
+    elif a==2 and c<60:
+        print("Your Ticket price has 30% discount : ", (Ticket)-(0.3)*Ticket)
+    else:
+        print("invalid information")
+    #break'''
+
+    
+
+
+'''while True:
+    Discount=int(input("Enter the discoutn without any symbols:"))
+    Ticket=1000
+    a=int(input(Gender :
+            1.Male
+            2.Female
+            Choose the gender:))
+    c=int(input("Enter your age: "))
+
+    if a==1 and c>=60:
+        print("Your Ticket price has discount :",(Ticket)-(Discount/100)*Ticket)
+    elif a==1 and c<60:
+        print("Your Ticket price has 0% discount : ",Ticket)
+    elif a==2 and c>=60:
+        print("Your Ticket price has discount : ", (Ticket)-(Discount/100)*Ticket)
+    elif a==2 and c<60:
+        print("Your Ticket price has discount : ", (Ticket)-(Discount/100)*Ticket)
+    else:
+        print("invalid information")
+    #break'''
+        
+        
